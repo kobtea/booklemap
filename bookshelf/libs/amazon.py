fa51841d4a044781758acb5b3b0fdad58d6b3049
kobtea <kobtea9696@gmail.com>
2014-05-04 02:04:56 +0900

    impl book searching from amazon api

---
+++ bookshelf/libs/amazon.py
@@ -0,0 +1,25 @@
+import amazonproduct
+from amazon_config import *  # noqa
+
+
+class API:
+    def __init__(self):
+        self.api = amazonproduct.API(cfg=AMAZON_CONFIG)
+        self.associate_tag = AMAZON_CONFIG['associate_tag']
+
+    def to_dict(self, elem, index):
+        d = {}
+        d['index'] = index  # used to sort
+        d['asin'] = elem.ASIN
+        d['title'] = unicode(elem.ItemAttributes.Title)
+        return d
+
+    def find_by_keyword(self, keyword):
+        # TODO: AWS.ECommerceService.NoExactMatches
+        items = self.api.item_search('Books', Keywords=keyword)
+        l = []
+        for i, elem in enumerate(items):
+            l.append(self.to_dict(elem, i))
+        return l
+
+api = API()
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .libs import amazon
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'bookshelf/index.html', {})
+
+
+def search(request):
+    q = request.GET.get('q')
+    item_dict = amazon.api.find_by_keyword(q)
+    meta = {'aso_tag': amazon.api.associate_tag}
+    # TODO: no matches handling
+    return render(request,
+                  'bookshelf/list.html',
+                  {'item_dict': item_dict,
+                   'meta': meta}
+                  )
