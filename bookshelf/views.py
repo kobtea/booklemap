fb000ef082e6b9bfe8c5d4b3434ed4a4ae692b63
kobtea <kobtea9696@gmail.com>
2014-05-04 10:23:23 +0900

    modify to redirect at search/post

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .libs import amazon
 from .models import Status
-
-from django.http import HttpResponse
 
 
 def index(request):
@@ -45,4 +43,5 @@ class Search(View):
                 status=status
             )
         status_obj.save()
-        return HttpResponse('hoge')
+        # TODO: refactor me
+        return redirect(request.META['HTTP_REFERER'])
